5ddd0c67f59a6b902ed0a7a9c429dfd7864fa926
Keita Shimizu <miz@shimizukeita-no-MacBook.local>
2018-01-14 21:06:50 +0900

    excluded book_style work / relogin added

--- crawl.py
+++ crawl.py
@@ -1,5 +1,5 @@
-# coding: utf-8
-
+#coding: utf-8
+# このドキュメントは日本語コメントが含まれています
 from pixivpy3 import *
 import json
 from time import sleep
@@ -20,6 +20,8 @@ client_info = json.load(f)
 f.close()
 api.login(client_info['pixiv_id'], client_info['password'])
 
+MAXIMUM_IMAGE_NUMBER = 100000 # If the directory has more than this number, stop crawling
+
 # ID Range
 #STARTID = 1500000
 #ENDID = 2000000
@@ -34,7 +36,7 @@ if not os.path.exists("./pixiv_images"):
 saving_direcory_path = './pixiv_images/'
 
 # Excluded tags. Add tags you don't want to download.
-exclude_tags = ["講座", "線画"]
+exclude_tags = ["講座","線画"]
 
 lst = list(range(STARTID, ENDID + 1))
 random.shuffle(lst)
@@ -69,32 +71,39 @@ for ID in lst:
         json_result.response.sort(key=lambda x: x.stats.score, reverse=True)
         total_works = json_result.pagination.total
 
-        #if not os.path.exists(saving_direcory_path):
-        #    os.mkdir(saving_direcory_path)
-
+        num_files = 0
         for work_no in range(0, total_works):
-            illust = json_result.response[work_no]
+            if num_files > MAXIMUM_IMAGE_NUMBER:
+                print("Exceeded maximum number.")
+                exit()
 
+            illust = json_result.response[work_no]
             skip_this_work = False
 
             # Check the score
             score = illust.stats.score
-            if score < 500:
+            if score < 1600:
                 debug_print("skip: low score")
                 skip_this_work = True
 
             # R18
             if illust.age_limit != "all-age":
                 debug_print("skip: HENTAI")
-                skip_this_work = True
+                #skip_this_work = True
 
             # Non-illustration work
             if illust.type != "illustration":
                 debug_print("skip: not illust")
                 skip_this_work = True
 
+            if illust.book_style != "none":
+                debug_print("skip: book_style")
+                skip_this_work = True
+
             # Having excluded tags
             for tag in exclude_tags:
+                target= ".*" + tag + ".*"
+                #r = re.compile(target)
                 r = re.compile(".*" + tag + ".*")
                 if any(r.match(i) for i in illust.tags):
                     debug_print("tag " + tag + " detected!!")
@@ -113,12 +122,15 @@ for ID in lst:
                 # Select image size to download
                 #aapi.download(illust.image_urls.px_128x128, saving_direcory_path) #small
                 aapi.download(illust.image_urls.px_480mw, saving_direcory_path) #medium
+                num_files += 1
                 #aapi.download(illust.image_urls.large, saving_direcory_path) #large
                 sleep(5)
 
-        print('\nThat\'s all.')
+        print('Artist download ended.')
+        print(separator)
     except:
         debug_print("exception")
         traceback.print_exc()
+        api.login(client_info['pixiv_id'], client_info['password'])
         sleep(1)
         continue
